44ab230f555520ac6100af2b79831d50ef7bbc07
Dermot reynolds <dermotreynolds@Dermots-MacBook-Pro.local>
2018-08-03 06:52:25 +0200

    first commit

--- main.tf
+++ main.tf
@@ -23,74 +23,80 @@ resource "azurerm_resource_group" "wfinit_resource_group" {
 
 #Create a storage account to put our state files into
 
-# resource "azurerm_storage_account" "wfinit_storage_account" {
-#   name                     = "${var.organisation}${var.department}${var.environment}010103"
-#   resource_group_name      = "${azurerm_resource_group.wfinit_resource_group.name}"
-#   location                 = "${azurerm_resource_group.wfinit_resource_group.location}"
-#   account_tier             = "Standard"
-#   account_replication_type = "GRS"
+resource "azurerm_storage_account" "wfinit_storage_account" {
+  name                     = "wfecom2020state"
+  resource_group_name      = "${azurerm_resource_group.wfinit_resource_group.name}"
+  location                 = "${azurerm_resource_group.wfinit_resource_group.location}"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 
-#   tags {
-#     environment  = "${var.environment}"
-#     department   = "${var.department}"
-#     organisation = "${var.organisation}"
+  tags {
+    environment  = "${var.environment}"
+    department   = "${var.department}"
+    organisation = "${var.organisation}"
+  }
+}
+
+#Create a container to put our state files into
+resource "azurerm_storage_container" "wfinit_storage_container" {
+  name                  = "wfecom2020state"
+  resource_group_name   = "${azurerm_resource_group.wfinit_resource_group.name}"
+  storage_account_name  = "${azurerm_storage_account.wfinit_storage_account.name}"
+  container_access_type = "private"
+}
+
+# data "azurerm_client_config" "current" {}
+
+
+# resource "azurerm_key_vault" "wfcore_key_vault" {
+#   name                = "${var.organisation}${var.department}${var.environment}-core"
+#   location            = "${azurerm_resource_group.wfinit_resource_group.location}"
+#   resource_group_name = "${azurerm_resource_group.wfinit_resource_group.name}"
+#   tenant_id           = "${data.azurerm_client_config.current.tenant_id}"
+
+
+#   access_policy {
+#     tenant_id = "${data.azurerm_client_config.current.tenant_id}"
+#     object_id = "${data.azurerm_client_config.current.service_principal_object_id}"
+
+
+#     key_permissions = [
+#       "create",
+#       "get",
+#       "list",
+#       "backup",
+#       "decrypt",
+#       "delete",
+#       "encrypt",
+#       "get",
+#       "import",
+#       "list",
+#       "purge",
+#       "recover",
+#       "restore",
+#       "sign",
+#       "unwrapKey",
+#       "update",
+#       "verify",
+#       "wrapKey",
+#     ]
+
+
+#     secret_permissions = [
+#       "backup",
+#       "delete",
+#       "get",
+#       "list",
+#       "purge",
+#       "recover",
+#       "set",
+#       "restore",
+#     ]
+#   }
+
+
+#   sku {
+#     name = "standard"
 #   }
 # }
 
-# #Create a container to put our state files into
-# resource "azurerm_storage_container" "wfinit_storage_container" {
-#   name                  = "${var.organisation}${var.department}${var.environment}state010101"
-#   resource_group_name   = "${azurerm_resource_group.wfinit_resource_group.name}"
-#   storage_account_name  = "${azurerm_storage_account.wfinit_storage_account.name}"
-#   container_access_type = "private"
-# }
-
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_key_vault" "wfcore_key_vault" {
-  name                = "${var.organisation}${var.department}${var.environment}-core"
-  location            = "${azurerm_resource_group.wfinit_resource_group.location}"
-  resource_group_name = "${azurerm_resource_group.wfinit_resource_group.name}"
-  tenant_id           = "${data.azurerm_client_config.current.tenant_id}"
-
-  access_policy {
-    tenant_id = "${data.azurerm_client_config.current.tenant_id}"
-    object_id = "${data.azurerm_client_config.current.service_principal_object_id}"
-
-    key_permissions = [
-      "create",
-      "get",
-      "list",
-      "backup",
-      "decrypt",
-      "delete",
-      "encrypt",
-      "get",
-      "import",
-      "list",
-      "purge",
-      "recover",
-      "restore",
-      "sign",
-      "unwrapKey",
-      "update",
-      "verify",
-      "wrapKey",
-    ]
-
-    secret_permissions = [
-      "backup",
-      "delete",
-      "get",
-      "list",
-      "purge",
-      "recover",
-      "set",
-      "restore",
-    ]
-  }
-
-  sku {
-    name = "standard"
-  }
-}
